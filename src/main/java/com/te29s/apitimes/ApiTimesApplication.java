e9c82a28dc4ca815563a9acb4e469b44c52040a9
Miguel <miguelsilvapeloso@gmail.com>
2024-12-14 19:41:15 -0300

    chors: core dependencies

---
+++ src/main/java/com/te29s/apitimes/ApiTimesApplication.java
@@ -0,0 +1,13 @@
+package com.te29s.apitimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTimesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTimesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/te29s/apitimes/ApiTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.te29s.apitimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTimesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
